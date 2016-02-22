a6fbb7cbbedd45bd53deef771e9613b15a2ea942
Narita Pandhe <narita.pandhe@gmail.com>
2016-02-22 15:58:51 -0500

    Basic get() implemented

--- smallish/smallish/Try.py
+++ smallish/smallish/Try.py
@@ -1,23 +1,44 @@
 """SimpleApp.py"""
 from itertools import groupby
 from pyspark import SparkContext
+import numpy as np
 
+sc = SparkContext("local", "Simple App")
 
-def groupByExample():
+def nonNegativeMod(x, mod):
+    rawMod = x % mod
+    rawMod = rawMod + (mod if (rawMod < 0) else 0)
+    return rawMod
+
+def getPartition(key):
+	return nonNegativeMod(hash(key),5)
+
+"""def groupByExample():
 	things = [("animal", "bear"), ("animal", "duck"), ("plant", "cactus"), ("vehicle", "speed boat"), ("vehicle", "school bus")]
 	for key, group in groupby(things, lambda x: x[0]):
-			print "%s : %s." % ( key, group)
+			print "%s : %s." % ( key, group)"""
     
-def getFromIndex(keyList):
-	ksByPartition = {}
-	for k, v in groupby(keyList, lambda x: x[1]):
-		#For every key in the get request, find its respective partition
-		#ksByPartition = ks.groupBy(k => partitioner.get.getPartition(k))
-		print "%s : %s." % (k, v)
+def partitionFunc(d):
+	return d
+  
+def getFromIndex(rdd2,keyList):
+	ksByPartition=[]
+	for k,v in keyList:
+		ksByPartition=getPartition(v)
+
+	print(ksByPartition)
+	partitions = ksByPartition
+	results = sc.runJob(rdd2, partitionFunc, [2], True)
+	
+	allResults = []
+	for k,v in keyList:
+		allResults.append(dict(results).get(v))
+
+	return allResults
 	
 
-def makeMap(item):
-	mapObject = ((k,v) for (k,v) in item)
+def makeMap(index,kv):
+	mapObject = ((k,v) for (k,v) in kv)
 	return (mapObject)
 
 def apply(elems):
@@ -27,14 +48,15 @@ def updatable(elems):
 	return updatable(elems,lambda id, a: a,lambda id, a, b: b)
 
 def updatable(elems, z = lambda K, U : V, f = lambda K, V, U : V):
+	#elemsPartitioned=elems.partitionBy(elems.count())
 	elemsPartitioned=elems.partitionBy(5)
-	partitions = elemsPartitioned.mapPartitions(makeMap)
+	elemsPartitioned.collect()
+	partitions = elemsPartitioned.mapPartitionsWithIndex((makeMap),True)
 	return (partitions)
     
 def main():
 
 	logFile = "README.md"  # Should be some file on your system
-	sc = SparkContext("local", "Simple App")
 	logData = sc.textFile(logFile).cache()
 
 	numAs = logData.filter(lambda s: 'a' in s).count()
@@ -42,16 +64,14 @@ def main():
 
 	
 	#Create an RDD of key-value pairs with Long keys.
-	rdd1 = sc.parallelize(range(50)).map(lambda x: (x, x + 1))
+	rdd1 = sc.parallelize(range(50)).map(lambda x: (x, x*x))
 	rdd2=apply(rdd1)
-	print("Elements Printed Here*******************************************************")
-	print(rdd2.take(5))
-	print("Elements Print Ends*******************************************************")
-
-	print("Group By Example*******************************************************")
-	groupByExample()
-	lis1=[(1,1), (2,2), (3,3)]
-	getFromIndex(lis1)
+	
+	print("GET Output *******************************************************")
+	list1=[(0,12)]
+	print(getFromIndex(rdd2,list1))
+	print("GET Output *******************************************************")
+	
 
 if __name__ == "__main__":
 	main()
