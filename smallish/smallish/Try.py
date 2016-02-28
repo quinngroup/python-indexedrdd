2438dcfbbf11c5b1812199f858104d110e533206
Narita Pandhe <narita.pandhe@gmail.com>
2016-02-28 02:53:41 -0500

    put() functionality partially done. Have to figure out zipPartitions

--- smallish/smallish/Try.py
+++ smallish/smallish/Try.py
@@ -5,6 +5,50 @@ import numpy as np
 
 sc = SparkContext("local", "Simple App")
 
+class MultiputZipper:
+    
+    def __init__(self, z = lambda (K, U) : V, f = lambda (K, V, U) : V):
+    	self.z=z
+    	self.f=f
+
+    def apply(thisIter, otherIter):
+    	print("in apply of multiput")		
+      #thisPart = thisIter.next()
+      #Iterator(thisPart.multiput(otherIter, z, f))
+
+def putInIndex(rdd2,elems):
+	return put1(elems)
+
+def put1(elems): 
+	return put2(elems,lambda id, a: a,lambda id, a, b: b)
+
+def put2(elems, z = lambda K, U : V, f = lambda K, V, U : V):
+	updates = sc.parallelize(elems).partitionBy(5)
+	print(updates.take(1))
+	print("Currying sample!!!!!")
+	f1(1)(2)(3,4,5)
+	print("Currying sample!!!!!")
+	zipPartitionsWithOther(updates)(MultiputZipper(z,f))
+
+def f1(a):
+    def g1(b):
+        def h1(c, d, e):
+            print(a, b, c, d, e)
+        return h1
+    return g1
+  
+def zipPartitionsWithOther(other):
+	def f2(f):
+		print("in f2")
+	return f2
+	print("Returned from f2")
+	partitioned = other.partitionBy(5)
+	newPartitionsRDD = partitionsRDD.zipPartitions(partitioned, true)(f)
+	#new IndexedRDD(newPartitionsRDD)
+   # newPartitionsRDD = partitionsRDD.zipPartitions(partitioned, true)(f)
+   # return newPartitionsRDD
+ 
+"""def OtherZipPartitionsFunction[V2, V3] = Function2[Iterator[IndexedRDDPartition[K, V]], Iterator[(K, V2)], Iterator[IndexedRDDPartition[K, V3]]]"""
 def nonNegativeMod(x, mod):
     rawMod = x % mod
     rawMod = rawMod + (mod if (rawMod < 0) else 0)
@@ -65,12 +109,19 @@ def main():
 	
 	#Create an RDD of key-value pairs with Long keys.
 	rdd1 = sc.parallelize(range(50)).map(lambda x: (x, x*x))
-	rdd2=apply(rdd1)
+	rdd2=apply(rdd1).cache()
 	
 	print("GET Output *******************************************************")
 	list1=[(0,12)]
 	print(getFromIndex(rdd2,list1))
 	print("GET Output *******************************************************")
+
+
+	print("PUT Output *******************************************************")
+	list1=[(1,10873)]
+	rdd3=putInIndex(rdd2,list1)
+	#print(rdd.take(20))
+	#print("PUT Output *******************************************************")
 	
 
 if __name__ == "__main__":
